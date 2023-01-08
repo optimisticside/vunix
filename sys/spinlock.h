9dbdb0e2ef43153eef5fa160affebc50c8dadbc7
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-07 19:52:21 -0800

    Clean up spinlock code

---
+++ sys/spinlock.h
@@ -0,0 +1,46 @@
+#ifndef _LOCK_H_
+#define _LOCK_H_
+
+/*
+ * Spin-locks involve CPUs looping indefinitely until the lock's value is set
+ * to false. They are used for any data that is shared across CPUs.
+ */
+struct spinlock {
+	int value;		/* Value of the lock */
+	struct cpu *holder;	/* CPU Holding lock */
+}
+
+static inline void spin_lock(struct spinlock *lk) {
+	if (lk->value)
+		panic("spin-lock acquire");
+
+	/* On RISC-V, sync_lock_test_and_set turns into an atomic swap: */
+ 	while(__sync_lock_test_and_set(&lk->locked, 1) != 0)
+		continue;
+
+	/*
+	 * Tell the C compiler and the CPU to not move loads or stores past
+	 * this point, to ensure all stores in the critical section are visible
+	 * to other CPUs before the lock is released, and that loads in the
+	 * critical section occur strictly before the lock is released.
+	 *
+	 * On RISC-V, this emits a fence instruction.
+	 */
+	__sync_synchronize();
+	lk->cpu = mycpu();
+}
+
+static inline void spin_unlock(struct spinlock *lk) {
+	lk->cpu = NULL;
+	__sync_synchronize();
+
+	/*
+	 * Release the lock, equivalent to lk->locked = 0.
+	 *
+	 * Doesn't use a C assignment, since the C standard implies that an
+	 * assignment might be implemented with multiple store instructions.
+	 */
+	__sync_lock_release(&lk->locked);
+}
+
+#endif // !_LOCK_H_
