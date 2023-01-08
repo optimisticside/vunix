0399c021d3a62d44caab3b78e1e455a6c1475be9
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-07 19:59:06 -0800

    Add definitions for thread & process tables

--- sys/lock.c
+++
@@ -1,5 +0,0 @@
-#include "types.h"
-#include "param.h"
-#include "lock.h"
-
-
--- sys/proc.h
+++ sys/proc.h
@@ -7,7 +7,7 @@
  * A process is defined as an independent image of execution in the operating
  * system. It is contained in its own address space.
  */
-struct proc {
+extern struct proc {
 	char state;			/* Current state */
 	uint8_t flags;			/* Additional flags */
 	int pid;			/* Unique identifier */
@@ -18,13 +18,13 @@ struct proc {
 	struct space *map;		/* Address space */
 	struct inode *cwd;		/* Current working directory */
 	struct file *ofile[NOFILE];	/* Open file handles */
-}
+} procs[NPROC];
 
 /*
  * A process is composed of threads, each of which have their own stacks (both
  * for user and kernel space).
  */
-struct thread {
+extern struct thread {
 	char state;			/* Current state */
 	uint16_t flags;			/* Additional flags */
 	int tid;			/* Unique identifier */
@@ -34,7 +34,7 @@ struct thread {
 	struct proc *proc;		/* Controlling process */
 	struct trapframe *tframe;	/* CPU state upon interrupt (for trampoline) */
 	struct context *ctx;		/* Where to switch() to to run thread */
-}
+} threads[NTHREAD];
 
 enum { TD_NONE, TD_READY, TD_SLEEP, TD_RUN };
 enum { P_NONE, P_EMBRYO, P_ALIVE, P_DEAD, P_ZOMBIE };
