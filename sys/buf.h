0b54ecf9d11e5eb3a1e3355c8870b0d83c965c34
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-15 12:11:16 -0800

    Implement buffer data-structure

---
+++ sys/buf.h
@@ -0,0 +1,21 @@
+#ifndef _BUF_H_
+#define _BUF_H_
+
+/*
+ * A buffer is the center of all I/O operations on block devices like
+ * disks. They also serve as a cache for data that is stored on the block
+ * device.
+ *
+ * The structure below is not actually the buffer, but the header of the
+ * buffer (the actual buffer comes right after this structure).
+ */
+extern struct buf {
+	int flags;		/* Additional flags */
+	struct buf *forw;	/* Next buffer in queue */
+	struct buf *prev;	/* Previous buffer in queue */
+	uint64_t blkno;		/* Block number on device */
+	int dev;		/* ID of device */
+	int error;		/* Error returned from I/O */
+} bufs[NBUF];
+
+#endif /* !_BUF_H_ */
