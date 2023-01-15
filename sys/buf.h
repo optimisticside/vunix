418dbbd4cdc15fedb78012ef945ccc4f3c232918
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-15 13:58:46 -0800

    Implement device-table structure & add buffer flags

--- sys/buf.h
+++ sys/buf.h
@@ -20,6 +20,29 @@ extern struct buf {
 	int error;		/* Error returned from I/O */
 } bufs[NBUF];
 
+/*
+ * Each block device has a device-table, which contrains private stuff and
+ * two doubly linked-lists that contain all buffers associated with that major
+ * device.
+ */
+struct devtab {
+	struct buf *head;	/* First buffer for this device */
+	struct buf *tail;	/* Last buffer for this device */
+	struct buf *iohead;	/* Head of I/O queue */
+	struct buf *iotail;	/* Tail of I/O queue */
+};
+
+#define	B_WRITE	0		/* Non-read pseudo-flag */
+#define	B_READ	1		/* Read when I/O occurs */
+#define	B_DONE	2		/* Transaction finished */
+#define	B_ERROR	4		/* Transaction aborted */
+#define	B_BUSY	8		/* Not on free list */
+#define	B_PHYS	16		/* Physical I/O */
+#define	B_WANTED 64		/* Issue wakeup when BUSY goes off */
+#define	B_RELOC	128		/* No longer used */
+#define	B_ASYNC	246		/* Don't wait for I/O completion */
+#define	B_DELWRI 512		/* Don't write till block leaves available list */
+
 struct buf *bread(int dev, uint64_t blkno, int size);
 void brelease(struct buf *bp);
 void bwrite(struct buf *bp);
