329248d93150f4313b416174e1feef4282d796c4
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-20 19:49:34 -0800

    Create I-Node structure

--- sys/inode.h
+++ sys/inode.h
@@ -1,4 +1,11 @@
 #ifndef _INODE_H_
 #define _INODE_H_
 
+/*
+ * An I-Node is the center of all file-system operations done. It represents a
+ * node in the file system, whether that be a directory, file, or some device.
+ */
+struct inode {
+}
+
 #endif /* !_INODE_H_ */
