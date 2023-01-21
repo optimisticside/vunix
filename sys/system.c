b32e8af86d5df761c8887572b2bd8acc8da29d6a
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-20 21:19:51 -0800

    Fix error about incomplete types

--- sys/system.c
+++ sys/system.c
@@ -1,6 +1,11 @@
 #include "types.h"
 #include "param.h"
 
+#include "buf.h"
+#include "cpu.h"
+#include "inode.h"
+#include "proc.h"
+
 struct buf bufs[NBUF];
 struct cpu cpus[NCPU];
 struct inode inodes[NINODE];
