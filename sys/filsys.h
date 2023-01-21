0c73109fcb9034c18e677705823ac651f8271b82
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-21 11:15:11 -0800

    Add superblock & improve dinode documentation

--- sys/filsys.h
+++ sys/filsys.h
@@ -5,7 +5,10 @@
 #include "param.h"
 
 /*
- * Structure of an I-Node as it resides on the disk.
+ * Structure of an I-Node as it resides on the disk. Holds 10 zones, 8 of which
+ * are direct pointers to blocks. The second to last one is an indirect pointer
+ * (a pointer to a block of pointers to blocks) and the last one is a doubly
+ * indirect pointer.
  */
 struct dinode {
 	uint16_t mode;		/* I-Node mode */
@@ -17,6 +20,25 @@ struct dinode {
 	uint32_t mtime;		/* Modification time */
 	uint32_t ctime;		/* Status-change time */
 	uint32_t zone[10];	/* Pointers to blocks */
+};
+
+/*
+ * Structure of the file-system super block as it resides on the disk. Usually
+ * stored in the first block of the disk that holds the file-system.
+ */
+struct superblock {
+	uint16_t ninodes;	/* Number of I-nodes */
+	uint16_t xx0;		/* Unused */
+	uint16_t impblks;	/* Number of I-Node bitmap blocks */
+	uint16_t zmpblks;	/* Number of zone bitmap blocks */
+	uint16_t logzsz;	/* Log2 of zone size */
+	uint16_t xx1;		/* Unused */
+	uint32_t maxsz;		/* Max file size */
+	uint32_t zones;		/* Number of zones */
+	uint16_t magic;		/* Magic number */
+	uint16_t xx2;		/* Unused */
+	uint16_t blksize;	/* Block size (bytes) */
+	uint8_t diskver;	/* Disk version */
 };
 
 #endif /* !_FILSYS_H_ */
