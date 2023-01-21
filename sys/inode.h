a60b7827ad21c8098ff9836e95b259df92e6855b
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-21 10:45:08 -0800

    Add fields in I-Node structure

--- sys/inode.h
+++ sys/inode.h
@@ -6,6 +6,14 @@
  * node in the file system, whether that be a directory, file, or some device.
  */
 struct inode {
+	int flags;		/* Additional flags */
+	char count;		/* Reference count */
+	int dev;		/* Device number */
+	uint64_t num;		/* I-node number on device */
+	short nlink;		/* Directory entries */
+	short uid;		/* Owner */
+	short gid;		/* Group of owner */
+	uint64_t size;		/* Size of file */
 }
 
 #endif /* !_INODE_H_ */
