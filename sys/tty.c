318af8aec046c4ea68595a62676c414eebdd46b4
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-22 19:18:23 -0800

    Use spinlocks for cblocks

--- sys/tty.c
+++ sys/tty.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "param.h"
 #include "proc.h"
+#include "lock.h"
 #include "conf.h"
 #include "tty.h"
 
@@ -11,28 +12,35 @@ struct cblock cbfreelist[];
  * space. The block-device equivalent to this is done in getblk().
  */
 void putc(int c, struct cblock *cb) {
+	acquire(&cb->lock);
 	while (cb->size == CBLKSIZ) {
 		if (cb->next == NULL) {
-			while ((cb->next = cballoc()))
+			while ((cb->next = cballoc())) {
 				sleep(cbfreelist);
+			}
 		}
+		release(&cb->lock);
 		cb = cb->next;
+		acquire(&cb->lock);
 	}
 	cb->chars[CBLKSIZ - cb->size++] = c;
+	release(&cb->lock);
 }
 
 /*
  * Writes a character to a character list.
  */
-int getc(struct chrblk *cb) {
+int getc(struct cblock *cb) {
 	char *cp;
 	char c;
 
+	acquire(&cb->lock);
 	if (cb->size == 0)
 		return '\0';
 	c = cb->chars[CBLKSIZ - cb->size--];
 	for (cp = &cb->chars[CBLKSIZ];
 		cp < &cb->chars[CBLKSIZ - cb->size]; cp++)
 		*cp = *(cp - 1);
+	release(&cp->lock);
 	return c;
 }
