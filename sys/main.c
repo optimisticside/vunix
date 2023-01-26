754c366142ff8ef90d4069ca4c94294b6cf6d061
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-25 17:03:24 -0800

    Implement I-node initialization routine

--- sys/main.c
+++ sys/main.c
@@ -9,10 +9,30 @@
 #include "tty.h"
 
 /*
+ * I-Node initialization routine. Reads the root-device's superblock and
+ * initializes the current date from the last modified date.
+ */
+void iinit(void) {
+	struct buf *cp, *bp;
+	struct thread *td;
+
+	td = mycpu()->thread;
+	blkdevs[major(rootdev)].open(rootdev);
+	bp = bread(rootdev, SUPERBLKNO);
+	cp = balloc();
+	if (td->error)
+		panic("iinit");
+	memcpy(bp->addr, cp->addr, sizeof(struct superblock *));
+	brelease(bp);
+	mount[0].buf = cp;
+	mount[0].dev = rootdev;
+}
+
+/*
  * Main entry point for kernel. Called by bootloader and other assembly code.
  * Hands contrl onto start() and ultimately the scheduler.
  */
-int main() {
+void main(void) {
 	if (cpuid() == 0)
 		start();
 	while (!started)
