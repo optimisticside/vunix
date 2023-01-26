8a161d1bbdbbfb1125b84f32e7df9c79899d8ecb
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-25 20:03:38 -0800

    Fix place of function prototypes

--- sys/print.h
+++ sys/print.h
@@ -5,6 +5,9 @@
 #include "param.h"
 #include "machdep.h"
 
+void putchar(int c);
+void printf(const char *fmt, ...);
+
 /*
  * A panic is when a non-recoverable error occurs in the kernel, and the only
  * thing that can be done is to spin forever.
@@ -14,8 +17,5 @@ __attribute__ ((noreturn)) static inline void panic(char *str) {
 	for (;;)
 		pause();
 }
-
-void putchar(int c);
-void printf(const char *fmt, ...);
 
 #endif /* !_PRINT_H_ */
