ee214f874d30ba9bd127e16b75fe1bc78148f1b6
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-28 22:14:24 -0800

    Implement string.h

---
+++ sys/string.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "param.h"
+#include "string.h"
+
+void *memcpy(char *to, char *from, size_t count) {
+	do
+		*to++ = *from++
+	while (--count);
+	return to;
+}
+
+void *memset(char *ptr, int val, size_t count) {
+	do
+		*to++ = val;
+	while (--count);
+	return to;
+}
---
+++ sys/string.h
@@ -0,0 +1,7 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+
+#endif /* _STRING_H_ */
