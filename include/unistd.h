e9d8961ba8d29e9461d5d187baed785c4c7a756e
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-29 13:46:05 -0800

    Add structs & more syscalls

--- include/unistd.h
+++ include/unistd.h
@@ -1,7 +1,12 @@
 #ifndef _UNISTD_H_
 #define _UNISTD_H_
 
+struct sgttyb;
+struct stat;
+
 int fork(void);
+int dup(int fd);
+int dup2(int from, int to);
 int read(int fd, void *buf, size_t count);
 int write(int fd, void *buf, size_t count);
 int open(char *pathname, int flags);
@@ -12,10 +17,13 @@ int unlink(char *link);
 int exec(char *path, char **argv);
 int exece(char *path, char **argv, char **envp);
 int chdir(char *path);
+size_t time(void);
 int alarm(int secs);
 int mknod(char *path, int mode, int dev);
 int chmod(char *path, int mode);
 int chown(char *path, int uid, int gid);
+int chroot(char *path);
+void *sbreak(size_t inc);
 int stat(char *path, struct stat *st);
 int fstat(int fd, struct stat *st);
 int seek(int fd, size_t off, int whence);
