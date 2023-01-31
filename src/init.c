c36cfde533354f64e89d5f7e83f75f977c8c7ba5
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-01-30 19:36:15 -0800

    Add daemon support

--- src/init.c
+++ src/init.c
@@ -1,6 +1,18 @@
 #include "unistd.h"
 
 /*
+ * Information each daemon is provided with. Currently this only includes
+ * arguments and no environment-variables (yet).
+ */
+char *dargv[] = { 0 };
+
+/*
+ * Path buffer for reading the paths of daemons' initialization files.
+ */
+#define NPBUF 64
+char pbuf[64] = { 0 };
+
+/*
  * Main system initialialization routine.
  */
 int main(int argc, char *argv[]) {
@@ -25,4 +37,25 @@ int main(int argc, char *argv[]) {
 	writefd = open("/dev/console", O_WRONLY);
 	dup2(writefd, 1);
 	dup2(writefd, 2);
+
+	/*
+	 * Read a list of daemons to spawn from /etc/init. While our approach
+	 * to do this is quite inefficient, it works.
+	 */
+	if ((initfd = open("/etc/init", O_RDONLY)) < 0)
+		return printf("No daemons found");
+	for (;;) {
+		for (pb = &hbuf[0]; pb < &pbuf[NPBUF] && *pb != '\0'; pb++) {
+			if (read(initfd, pb, 1) == '\n')
+				break;
+		}
+		if (bp != '\n' || pb != '\0')
+			return printf("Path buffer overflow");
+		patbuf[pb++] = '\0';
+		if (fork() == 0) {
+			close(initfd);
+			exec(pathbuf, dargv);
+		} else
+			printf("Starting %s\n");
+	}
 }
