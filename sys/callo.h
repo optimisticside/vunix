366bc147c1f5588b9e016dc9ee5af831b49cc513
optimisticside <47266822+optimisticside@users.noreply.github.com>
2023-03-28 09:11:42 -0700

    Clean up callout data-structure

--- sys/callo.h
+++ sys/callo.h
@@ -2,17 +2,17 @@
 #define _CALLO_H_
 
 #include "types.h"
+#include "lock.h"
 
 /*
  * A callout is a function that will be called within a certain time-interval.
- * Note that the function must be able to accept parameters pushed onto the
- * the stack, as that is the only way we can actually pass paramters to it.
+ * Note that the function can only take one argument.
  */
 extern struct callout {
+	struct spinlock lock;	/* Spin lock */
 	size_t time;		/* Incremental time */
-	int nargs;		/* Argument count */
-	void *args[];		/* Function arguments */
-	void (*func)();		/* Routine to call */
+	void *arg;		/* Argument to provide to function */
+	void (*func)(void *);	/* Routine to call */
 } callouts;
 
 #endif /* !_CALLO_H_ */
